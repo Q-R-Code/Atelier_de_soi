360b1d8f7cea6b2a75e35491f500d0cc4edebcb2
Quentin RUFFIER <qruffier@gmail.com>
2021-08-18 16:00:11 +0200

    MAJ Templates

--- atelier_de_soi/blog/views.py
+++ atelier_de_soi/blog/views.py
@@ -4,10 +4,6 @@ from django.views.generic import ListView
 from .models import BlogPost
 
 
-def home_blog(request):
-    return render(request, 'blog/home_blog.html')
-
-
 class BlogHome(ListView):
     model = BlogPost
     context_object_name = "posts"
