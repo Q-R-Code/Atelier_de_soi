8ab20853d659e578325490b2e26a8363df738bdb
Quentin RUFFIER <qruffier@gmail.com>
2021-08-25 15:34:44 +0200

    Adding branch dev and travis.py

---
+++ atelier_de_soi/atelier_de_soi_app/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
