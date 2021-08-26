6da4ab3b5758c7c8eda4b94cf1ab5c3fee6c0282
Q-R-Code <qruffier@gmail.com>
2021-08-26 19:51:14 +0200

    test settings

--- atelier_de_soi_app/settings/__init__.py
+++ atelier_de_soi_app/settings/__init__.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = (str(BASE_DIR.joinpath('staticfiles')),)
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
