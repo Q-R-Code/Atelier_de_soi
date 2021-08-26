4bc7a3156d5d7d23e48a15ef4e0fcad3b044b922
Quentin RUFFIER <qruffier@gmail.com>
2021-08-26 16:54:41 +0200

    Maj Procfile

--- atelier_de_soi_app/settings/__init__.py
+++ atelier_de_soi_app/settings/__init__.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
