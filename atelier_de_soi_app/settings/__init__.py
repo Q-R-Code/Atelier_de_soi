0e95bcfd365b8ebbe63799104ea22e951af2a10d
Q-R-Code <qruffier@gmail.com>
2021-08-26 19:58:06 +0200

    test settings

--- atelier_de_soi_app/settings/__init__.py
+++ atelier_de_soi_app/settings/__init__.py
@@ -14,7 +14,8 @@ import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if os.environ.get('ENV') == 'PRODUCTION':
     SECRET_KEY = os.environ.get('DJANGO_KEY')
@@ -122,11 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = (str(BASE_DIR.joinpath('staticfiles')),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
