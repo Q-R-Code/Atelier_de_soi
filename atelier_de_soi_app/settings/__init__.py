7af75f982f4c198205d69aabe82b810b9aa8c89e
Quentin RUFFIER <qruffier@gmail.com>
2021-08-26 15:44:06 +0200

    Maj settings

--- atelier_de_soi_app/settings/__init__.py
+++ atelier_de_soi_app/settings/__init__.py
@@ -15,16 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8fm7ht7gufbunt_3(t27j1(1bdxm62$6rr+$@7nab$a-nsf4!r'
+if os.environ.get('ENV') == 'PRODUCTION':
+    SECRET_KEY = os.environ.get('DJANGO_KEY')
+    DEBUG = False
+else:
+    SECRET_KEY = 'django-insecure-8fm7ht7gufbunt_3(t27j1(1bdxm62$6rr+$@7nab$a-nsf4!r'
+    DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'atelierdesoi.herokuapp.com']
 
 # Application definition
 
@@ -122,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT = (str(BASE_DIR.joinpath('staticfiles')),)
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
